2cb8fda0e32b91610a947b5949c1a662bd4bc903
Sebastian Nozzi <sebnozzi@gmail.com>
2015-01-26 10:07:31 +0100

    More tests

--- src/test/scala/org/scalavienna/refactoringdojo/gameoflife/AcceptanceTests.scala
+++ src/test/scala/org/scalavienna/refactoringdojo/gameoflife/AcceptanceTests.scala
@@ -9,12 +9,12 @@ import scala.Array.fallbackCanBuildFrom
 /**
  * As a general rule, don't remove test-cases. You are free to add, though.
  * However, consider these as acceptance tests. No need to be exhaustive here.
- * 
+ *
  * Rely on your own unit-tests for in-depth coverage of your components.
- * */
+ */
 class AcceptanceTests extends FunSuite with Matchers with AcceptanceTester {
 
-  test("An isolated cell dies") {
+  test("A lone cell (no neighbours) dies") {
     assertGoL(
       in = """ ....
                .O..
@@ -50,7 +50,7 @@ class AcceptanceTests extends FunSuite with Matchers with AcceptanceTester {
                ....""")
   }
 
-  test("Acceptance test (covers all rules)") {
+  test("A cell with 3 neighbours lives") {
     assertGoL(
       in = """ O...
                .O..
@@ -61,47 +61,71 @@ class AcceptanceTests extends FunSuite with Matchers with AcceptanceTester {
                .O..
                ....""")
   }
+  
+  test("A cell with more than 3 neighbours dies") {
+    assertGoL(
+      in = """ ....
+               .O..
+               OOO.
+               .O..""",
+      exp = """....
+               OOO.
+               O.O.
+               OOO.""")
+  }
+  
+  test("A cell is born if it had 3 neighbours") {
+    assertGoL(
+      in = """ O...
+               .O..
+               O...
+               ....""",
+      exp = """....
+               OO..
+               ....
+               ....""")
+  }
 
 }
 
 /**
- * You may only change the *implementation* of the "calculate" 
+ * You may only change the *implementation* of the "calculate"
  * method, NOT the type signature. Don't add methods here.
  */
 trait TestGameCalculator {
-  
+
   /**
    * This method is the bridge between the input
    * of the acceptance-tests and your real implementation
    * of the game-of-life algorithm.
-   * 
+   *
    * It should accept a String like as in the in = """...
-   * and produce a next iteration as expressed in the 
-   * exp = """... 
+   * and produce a next iteration as expressed in the
+   * exp = """...
    * ("in" stands for "input", "exp" stands for "expected").
-   *  
+   *
    * You may change the code inside this method (you
    * will actually have to). But leave the signature as-is.
-   * 
-   * Try to keep this method as anemic as possible, 
+   *
+   * Try to keep this method as anemic as possible,
    * delegating as much as possible to your own
    * components.
-   * 
+   *
    * Ideally, it should consist of just one or two lines.
-   * */
+   */
   def calculate(in: String): String = {
     val world = new GoL()
-    world.from_string ( in )
+    world.from_string(in)
     world.calcNxtIter
     world.to_str
   }
-  
+
 }
 
 /**
  * You should not need to change anything here.
  * Try to leave this trait as-is. It's an implementation detail.
- * */
+ */
 trait AcceptanceTester extends TestGameCalculator { self: FunSuite with Matchers =>
 
   private def parse(str: String): Seq[String] = {
